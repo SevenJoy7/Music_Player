37e03a58dfc9664fe42031a08858bcafb3aadcea
SevenJoy7 <Godlovebayvo1995@gmail.com>
2022-06-20 10:02:49 +0700

    up

---
+++ data/songPlaylists.js
@@ -0,0 +1,463 @@
+
+var listSong = [
+    [
+        {
+            name: "Thi Thiên 150",
+            singer: ["NissiWorship"],
+            path: "./assets/music/listSong1/song1.mp3",
+            image: "./assets/img/music/listSong1/song1.jpg"
+        },
+        {
+            name: "Vua Thánh",
+            singer: ["Hoàng Đức"],
+            path: "./assets/music/listSong1/song2.mp3",
+            image: "./assets/img/music/listSong1/song2.jpg"
+        },
+        {
+            name: "Xin Kéo Tôi Lại Gần Hơn",
+            singer: ["Hoàng Đức", "SRT Shine"],
+            path: "./assets/music/listSong1/song3.mp3",
+            image: "./assets/img/music/listSong1/song3.jpg"
+        },
+        {
+            name: "Chúa Ở Bên Con",
+            singer: ["Rina Kpă", "SRT Shine"],
+            path: "./assets/music/listSong1/song4.mp3",
+            image: "./assets/img/music/listSong1/song4.jpg"
+        },
+        {
+            name: "Tình Yêu Tuyệt Vời",
+            singer: ["SeVoice"],
+            path: "./assets/music/listSong1/song5.mp3",
+            image: "./assets/img/music/listSong1/song5.jpg"
+        },
+        {
+            name: "Gốc Nho Và Nhánh",
+            singer: ["Khánh Linh"],
+            path: "./assets/music/listSong1/song6.mp3",
+            image: "./assets/img/music/listSong1/song6.jpg"
+        },
+        {
+            name: "Trở Về",
+            singer: ["Y Jalin"],
+            path: "./assets/music/listSong1/song7.mp3",
+            image: "./assets/img/music/listSong1/song7.jpg"
+        },
+        {
+            name: "Vui Mừng Mỗi Ngày",
+            singer: ["H. Buffum","Rina Kpa, Liu Liu, Oanh Ory"],
+            path: "./assets/music/listSong1/song8.mp3",
+            image: "./assets/img/music/listSong1/song8.jpg"
+        },
+        {
+            name: "Thi Thiên 23",
+            singer: ["Isaac Thái"],
+            path: "./assets/music/listSong1/song9.mp3",
+            image: "./assets/img/music/listSong1/song9.jpg"
+        },
+        {
+            name: "Riêng Cho Ngài",
+            singer: ["David Boo"],
+            path: "./assets/music/listSong1/song10.mp3",
+            image: "./assets/img/music/listSong1/song10.jpg"
+        },
+        {
+            name: "Con Là Đền Thờ",
+            singer: ["Grace KLong"],
+            path: "./assets/music/listSong1/song11.mp3",
+            image: "./assets/img/music/listSong1/song11.jpg"
+        },
+        {
+            name: "Thi Thiên 61",
+            singer: ["Minh Châu"],
+            path: "./assets/music/listSong1/song12.mp3",
+            image: "./assets/img/music/listSong1/song12.jpg"
+        },
+        {
+            name: "Bước Đi Cùng Ngài (WALK WITH THE LORD)",
+            singer: ["Minh Châu"],
+            path: "./assets/music/listSong1/song13.mp3",
+            image: "./assets/img/music/listSong1/song13.jpg"
+        },
+        {
+            name: "Trong Danh Chúa Jesus",
+            singer: ["Tuấn Tú, Bun Shine"],
+            path: "./assets/music/listSong1/song14.mp3",
+            image: "./assets/img/music/listSong1/song14.jpg"
+        },
+        {
+            name: "Muôn Thu Không Đổi Thay",
+            singer: ["NissiWorship"],
+            path: "./assets/music/listSong1/song15.mp3",
+            image: "./assets/img/music/listSong1/song15.jpg"
+        },
+        {
+            name: "Mọi Sự Đã Được Trọn",
+            singer: ["NissiWorship"],
+            path: "./assets/music/listSong1/song16.mp3",
+            image: "./assets/img/music/listSong1/song16.jpg"
+        },
+        {
+            name: "Chúc Tôn Vua Vinh Hiển",
+            singer: ["Nissi Live Worship"],
+            path: "./assets/music/listSong1/song17.mp3",
+            image: "./assets/img/music/listSong1/song17.jpg"
+        },
+        {
+            name: "Ngài Chờ Anh Đó",
+            singer: ["Hoàng Đức"],
+            path: "./assets/music/listSong1/song18.mp3",
+            image: "./assets/img/music/listSong1/song18.jpg"
+        },
+        {
+            name: "TẠ ƠN CHA YÊU THƯƠNG",
+            singer: ["SHALOM Duy"],
+            path: "./assets/music/listSong1/song19.mp3",    
+            image: "./assets/img/music/listSong1/song19.jpg"
+        },
+        {
+            name: "Đức Ái Tâm Ngài",
+            singer: ["Isaac Thái"],
+            path: "./assets/music/listSong1/song20.mp3",
+            image: "./assets/img/music/listSong1/song20.jpg"
+        },
+        {
+            name: "Sư Tử Phái Judah",
+            singer: ["Isaac Thái"],
+            path: "./assets/music/listSong1/song21.mp3",
+            image: "./assets/img/music/listSong1/song21.jpg"
+        },
+    ],
+    [
+        {
+            name: "Hãy Tin Nhận Ngài",
+            singer: ["Quynh Mint"],
+            path: "./assets/music/listSong2/song1.mp3",
+            image: "./assets/img/music/listSong2/song1.jpg"
+        },
+        {
+            name: "Tin Lành Cứu Tội Nhân",
+            singer: ["A Huy"],
+            path: "./assets/music/listSong2/song2.mp3",
+            image: "./assets/img/music/listSong2/song2.jpg"
+        },
+        {
+            name: "Dù Con Ra Sao Cha Vẫn Yêu",
+            singer: ["David Boo"],
+            path: "./assets/music/listSong2/song3.mp3",
+            image: "./assets/img/music/listSong2/song3.jpg"
+        },
+        {
+            name: "Tình Yêu Chữa Lành",
+            singer: ["Thiên Ngôn"],
+            path: "./assets/music/listSong2/song4.mp3",
+            image: "./assets/img/music/listSong2/song4.jpg"
+        },
+        {
+            name: "Tìm về Cha, tìm về tình yêu",
+            singer: ["Lưu Chí Vỹ"],
+            path: "./assets/music/listSong2/song5.mp3",
+            image: "./assets/img/music/listSong2/song5.jpg"
+        },
+        {
+            name: "Xin Dâng Chúa Môi Miệng Con",
+            singer: ["Quốc Khánh"],
+            path: "./assets/music/listSong2/song6.mp3",
+            image: "./assets/img/music/listSong2/song6.jpg"
+        },
+        {
+            name: "Khi Bạn Thất Vọng Hãy Tìm Đến Chúa",
+            singer: ["Jonathan Duc"],
+            path: "./assets/music/listSong2/song7.mp3",
+            image: "./assets/img/music/listSong2/song7.jpg"
+        },
+        {
+            name: "Ngoài Chúa Ra Con Chẳng Là Chi ",
+            singer: ["Đạt Nguyễn"],
+            path: "./assets/music/listSong2/song8.mp3",
+            image: "./assets/img/music/listSong2/song8.jpg"
+        },
+        {
+            name: "Người Yêu Tôi Là Jesus",
+            singer: ["Đỗ Phú Quý"],
+            path: "./assets/music/listSong2/song9.mp3",
+            image: "./assets/img/music/listSong2/song9.jpg"
+        },
+        {
+            name: "Tìm",
+            singer: ["Nguyễn Hoàng Nam"],
+            path: "./assets/music/listSong2/song10.mp3",
+            image: "./assets/img/music/listSong2/song10.jpg"
+        },
+        {
+            name: "Chúa Thấu Hiểu Lòng Con",
+            singer: ["Quỳnh Mint"],
+            path: "./assets/music/listSong2/song11.mp3",
+            image: "./assets/img/music/listSong2/song11.jpg"
+        },
+        {
+            name: "Thuyền Con Neo Bến Nơi Jesus",
+            singer: ["Nguyễn Đức Trọng"],
+            path: "./assets/music/listSong2/song12.mp3",
+            image: "./assets/img/music/listSong2/song12.jpg"
+        },
+        {
+            name: "Lối Đi Nào Cho Ta",
+            singer: ["Lâm Phạm"],
+            path: "./assets/music/listSong2/song13.mp3",
+            image: "./assets/img/music/listSong2/song13.jpg"
+        },
+        {
+            name: "Ngài Là Chúa Các Chúa",
+            singer: ["Minh Châu"],
+            path: "./assets/music/listSong2/song14.mp3",
+            image: "./assets/img/music/listSong2/song14.jpg"
+        },
+        {
+            name: "Về Nơi Yêu Thương",
+            singer: ["Quốc Khánh"],
+            path: "./assets/music/listSong2/song15.mp3",
+            image: "./assets/img/music/listSong2/song15.jpg"
+        },
+        {
+            name: "Bài Ca Phúc Âm",
+            singer: ["DANIEL Thần"],
+            path: "./assets/music/listSong2/song16.mp3",
+            image: "./assets/img/music/listSong2/song16.jpg"
+        },
+        {
+            name: "Xin Chúa Cứu Gia Đình Con",
+            singer: ["Isaac Thái"],
+            path: "./assets/music/listSong2/song17.mp3",
+            image: "./assets/img/music/listSong2/song17.jpg"
+        },
+        {
+            name: "Như Chính Con Đây",
+            singer: ["Tiên Ê Ca"],
+            path: "./assets/music/listSong2/song18.mp3",
+            image: "./assets/img/music/listSong2/song18.jpg"
+        },
+        {
+            name: "Con Sẽ Không Sợ",
+            singer: ["Phạm Đình Thái Ngân"],
+            path: "./assets/music/listSong2/song19.mp3",
+            image: "./assets/img/music/listSong2/song19.jpg"
+        },
+        {
+            name: "Dâng Chúa Đời Con",
+            singer: ["Isaac Thái "],
+            path: "./assets/music/listSong2/song20.mp3",
+            image: "./assets/img/music/listSong2/song20.jpg"
+        },
+    ],
+    [
+        {
+            name: "Same God",
+            singer: ["Elevation Worship"],
+            path: "./assets/music/listSong3/song1.mp3",
+            image: "./assets/img/music/listSong3/song1.jpg"
+        },
+        {
+            name: "Above All / There Is None Like You",
+            singer: ["Lenny LeBlan"],
+            path: "./assets/music/listSong3/song2.mp3",
+            image: "./assets/img/music/listSong3/song2.jpg"
+        },
+        {
+            name: "Our God (Live)",
+            singer: ["Chris Tomlin"],
+            path: "./assets/music/listSong3/song3.mp3",
+            image: "./assets/img/music/listSong3/song3.jpg"
+        },
+        {
+            name: "10,000 Reasons (Bless the Lord)",
+            singer: ["Matt Redman"],
+            path: "./assets/music/listSong3/song4.mp3",
+            image: "./assets/img/music/listSong3/song4.jpg"
+        },
+        {
+            name: "Jesus I Need You",
+            singer: ["Hillsong Worship"],
+            path: "./assets/music/listSong3/song5.mp3",
+            image: "./assets/img/music/listSong3/song5.jpg"
+        },
+    
+        {
+            name: "Way Maker",
+            singer: ["Leeland"],
+            path: "./assets/music/listSong3/song6.mp3",
+            image: "./assets/img/music/listSong3/song6.jpg"
+        },
+        {
+            name: "Forever Amen (Live) ",
+            singer: ["Abundant Life Worship"],
+            path: "./assets/music/listSong3/song7.mp3",
+            image: "./assets/img/music/listSong3/song7.jpg"
+        },
+        {
+            name: "Goodness of God",
+            singer: ["Feat. Michael Bethany", "Gateway Worship"],
+            path: "./assets/music/listSong3/song8.mp3",
+            image: "./assets/img/music/listSong3/song8.jpg"
+        },
+        {
+            name: "One Thing",
+            singer: ["Hillsong Worship"],
+            path: "./assets/music/listSong3/song9.mp3",
+            image: "./assets/img/music/listSong3/song9.jpg"
+        },
+        {
+            name: "Grace To Grace",
+            singer: ["Hillsong Worship"],
+            path: "./assets/music/listSong3/song10.mp3",
+            image: "./assets/img/music/listSong3/song10.jpg"
+        },
+        {
+            name: "Here As In Heaven",
+            singer: ["Elevation Worship"],
+            path: "./assets/music/listSong3/song11.mp3",
+            image: "./assets/img/music/listSong3/song11.jpg"
+        },
+         {
+            name: "Wake",
+            singer: ["Hillsong Y&F Live"],
+            path: "./assets/music/listSong3/song12.mp3",
+            image: "./assets/img/music/listSong3/song12.jpg"
+        },
+        {
+            name: "Turn It Up",
+            singer: ["Planetshakers"],
+            path: "./assets/music/listSong3/song13.mp3",
+            image: "./assets/img/music/listSong3/song13.jpg"
+        },
+        {
+            name: "This Is Our Time",
+            singer: ["Planetshakers"],
+            path: "./assets/music/listSong3/song14.mp3",
+            image: "./assets/img/music/listSong3/song14.jpg"
+        },
+        {
+            name: "Good Grace",
+            singer: ["Hillsong UNITED"],
+            path: "./assets/music/listSong3/song15.mp3",
+            image: "./assets/img/music/listSong3/song15.jpg"
+        },
+        {
+            name: "Oceans (Where Feet May Fail)",
+            singer: ["Hillsong UNITED"],
+            path: "./assets/music/listSong3/song16.mp3",
+            image: "./assets/img/music/listSong3/song16.jpg"
+        },
+    ],
+    [
+        {
+            name: "Em Là Người Chúa Ban",
+            singer: ["BGM Thắng"],
+            path: "./assets/music/listSong4/song1.mp3",
+            image: "./assets/img/music/listSong4/song1.jpg"
+        },
+        {
+            name: "Chuyện Tình A-đam Và E-va",
+            singer: ["Lơ Thành"],
+            path: "./assets/music/listSong4/song2.mp3",
+            image: "./assets/img/music/listSong4/song2.jpg"
+        },
+        {
+            name: "Ước Nguyện Tình Yêu",
+            singer: ["Thiên Ngôn"],
+            path: "./assets/music/listSong4/song3.mp3",
+            image: "./assets/img/music/listSong4/song3.jpg"
+        },
+        {
+            name: "Bước Trong Tình Yêu",
+            singer: ["Cs Siu Y Kao & Cs Lê Nguyệt Anh"],
+            path: "./assets/music/listSong4/song4.mp3",
+            image: "./assets/img/music/listSong4/song4.jpg"
+        },
+        {
+            name: "Lời Nguyện Ước",
+            singer: ["Nguyễn Đức Trọng"],
+            path: "./assets/music/listSong4/song5.mp3",
+            image: "./assets/img/music/listSong4/song5.jpg"
+        },
+    
+        {
+            name: "Tình Yêu Tuyệt Đối",
+            singer: ["Isaac Thái ft Quỳnh Mint"],
+            path: "./assets/music/listSong4/song6.mp3",
+            image: "./assets/img/music/listSong4/song6.jpg"
+        },
+        {
+            name: "Khúc Tạ Ơn Ngày Cưới",
+            singer: ["Lơ Thành"],
+            path: "./assets/music/listSong4/song7.mp3",
+            image: "./assets/img/music/listSong4/song7.jpg"
+        },
+        {
+            name: "Bức Thư Gửi Em",
+            singer: ["Shyllo Thọ"],
+            path: "./assets/music/listSong4/song8.mp3",
+            image: "./assets/img/music/listSong4/song8.jpg"
+        },
+        {
+            name: "Người Nữ Của Chúa",
+            singer: ["Phạm Đình Thái Ngân"],
+            path: "./assets/music/listSong4/song9.mp3",
+            image: "./assets/img/music/listSong4/song9.jpg"
+        },
+        {
+            name: "Người Nữ Đến Từ Nơi Cha",
+            singer: ["Phạm Đình Thái Ngân, Phương Lý"],
+            path: "./assets/music/listSong4/song10.mp3",
+            image: "./assets/img/music/listSong4/song10.jpg"
+        },
+        {
+            name: "Chân Ái Chúa Ban",
+            singer: ["Isaac Thái, Lê Phương Lý, SRT Shine"],
+            path: "./assets/music/listSong4/song11.mp3",
+            image: "./assets/img/music/listSong4/song11.jpg"
+        },
+        {
+            name: "Giây Phút Mong Chờ",
+            singer: ["Minh Châu, Phương Nam"],
+            path: "./assets/music/listSong4/song12.mp3",
+            image: "./assets/img/music/listSong4/song12.jpg"
+        },
+        {
+            name: "Vòng Tay Mẹ Yêu",
+            singer: ["Lơ Thành"],
+            path: "./assets/music/listSong4/song13.mp3",
+            image: "./assets/img/music/listSong4/song13.jpg"
+        },
+         {
+            name: "Cám Ơn Cha Mẹ",
+            singer: ["Isaac Thái"],
+            path: "./assets/music/listSong4/song14.mp3",
+            image: "./assets/img/music/listSong4/song14.jpg"
+        },
+        {
+            name: "Yêu Mẹ",
+            singer: ["Thiên Ngôn"],
+            path: "./assets/music/listSong4/song15.mp3",
+            image: "./assets/img/music/listSong4/song15.jpg"
+        },
+        {
+            name: "Con yêu Cha Mẹ Nhiều",
+            singer: ["BGM Thắng"],
+            path: "./assets/music/listSong4/song16.mp3",
+            image: "./assets/img/music/listSong4/song16.jpg"
+        },
+         {
+            name: "Bài Ca Yêu Thương",
+            singer: ["Phương Nam, Minh Châu"],
+            path: "./assets/music/listSong4/song17.mp3",
+            image: "./assets/img/music/listSong4/song17.jpg"
+        },
+    ],
+];
+
+const MUSIC_STORAGE_KEY = 'VIK_MUSIC';
+
+localStorage.setItem(MUSIC_STORAGE_KEY, JSON.stringify(listSong));
+
+
